2e272a0ef942388e4a725f090e8679184f28c7ab
dhanuja97 <54883413+dhanuja97@users.noreply.github.com>
2019-09-13 09:19:46 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,21 @@
+x=-3
+
+if x>0.0:
+ print('positive')
+
+elif x<0.0:
+ print('negative')
+ x=-1.0*x
+else:
+ print('zero') 
+
+if x>0.0:
+ print('+ve')
+
+else:
+ if x<0.0:
+  print('-ve')
+  x=-1.0*x
+ else:
+  print('0') 
+
